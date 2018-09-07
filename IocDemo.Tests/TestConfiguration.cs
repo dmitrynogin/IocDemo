15e7e2e186720fa562620330a3b4421e26df64aa
Dmitry Nogin <dmitryn@mail.com>
2018-09-07 12:15:47 -0700

    Add IConfiguration support

---
+++ IocDemo.Tests/TestConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using IocDemo.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IocDemo.Tests
+{
+    [Service]
+    class TestConfiguration : IConfiguration
+    {
+        static readonly IEnumerable<(string Section, string Name, string Value)> Values = new[]
+        {
+            ("DB", "ConnectionString", "USER=John Doe;PASSWORD=qwerty")
+        };
+
+        public IEnumerable<(string Name, string Value)> Section(string section) =>
+            from v in Values
+            where v.Section == section
+            select (v.Name, v.Value);
+    }
+}
